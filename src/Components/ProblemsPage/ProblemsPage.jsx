a710c9d08a703056a7b43b24362f91e8493ddc5d
Priyanshu Sharma <priyanshu160101@gmail.com>
2023-08-15 18:03:43 +0530

    removed react-dropdown-select dependency

--- src/Components/ProblemsPage/ProblemsPage.jsx
+++ src/Components/ProblemsPage/ProblemsPage.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom'
-import Select from "react-dropdown-select";
+
 
 import "./ProblemsPage.css"
 import {backendUrl} from "../../constants.js";
